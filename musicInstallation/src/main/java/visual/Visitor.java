8288b266561fd35b7b6e6db96bdabe1d7dfe3b96
andrasSagi <andras.sagi8128@gmail.com>
2018-10-11 10:21:07 +0200

    Make visitors wait while inspecting showpieces

--- musicInstallation/src/main/java/visual/Visitor.java
+++ musicInstallation/src/main/java/visual/Visitor.java
@@ -1,8 +1,17 @@
+package visual;
+
 import javafx.scene.image.Image;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Visitor extends Entity {
 
-    private Entity target = exhibition.getExit();
+    private boolean waiting = false;
+    private final float waitingLimit = 180;
+    private float waitingTimer = 0;
+    private Target target;
+    private List<Target> targets = new ArrayList<>(exhibition.getShowPieces());
 
     public Visitor(double x, double y, Exhibition exhibition) {
         super(exhibition);
@@ -10,18 +19,34 @@ public class Visitor extends Entity {
         setY(y);
         setImage(new Image("visitor2.png"));
         exhibition.addVisitor(this);
+        targets.add(exhibition.getExit());
+        target = targets.get(0);
     }
 
-    public void step() {
-        setX(target.getX() > getX() ? getX() + 0.7 : getX() - 0.7);
-        setY(target.getY() > getY() ? getY() + 0.7 : getY() - 0.7);
-        setRotate(getAngle());
-        if (getBoundsInParent().intersects(target.getBoundsInParent())) {
-            destroy();
+    void step() {
+        if (waiting) {
+            waitingTimer += 1;
+            if (waitingTimer >= waitingLimit) {
+                waiting = false;
+            }
+        } else {
+            setX(target.getX() > getX() ? getX() + 0.7 : getX() - 0.7);
+            setY(target.getY() > getY() ? getY() + 0.7 : getY() - 0.7);
+            setRotate(getAngle());
+            if (getBoundsInParent().intersects(target.getBoundsInParent())) {
+                if (target instanceof Exit) {
+                    destroy();
+                } else {
+                    targets.remove(target);
+                    target = targets.get(0);
+                    waiting = true;
+                    waitingTimer = 0;
+                }
+            }
         }
     }
 
-    public void destroy() {
+    private void destroy() {
         exhibition.getChildren().remove(this);
         exhibition.addExVisitor(this);
     }
