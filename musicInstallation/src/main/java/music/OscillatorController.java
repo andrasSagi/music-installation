9c0819ed713d3f525aa3bfea8f1340c5b90e27d9
andrasSagi <andras.sagi8128@gmail.com>
2018-10-29 12:06:30 +0100

    Add fields and constructor ro OscillatorController

--- musicInstallation/src/main/java/music/OscillatorController.java
+++ musicInstallation/src/main/java/music/OscillatorController.java
@@ -1,4 +1,26 @@
 package music;
 
+import net.beadsproject.beads.core.AudioContext;
+import net.beadsproject.beads.data.Buffer;
+import net.beadsproject.beads.ugens.WavePlayer;
+
 public class OscillatorController {
+
+    private Oscillator oscillator;
+    private BeadsGenerator beadsGenerator;
+    private WavePlayer wavePlayer;
+    AudioContext audioContext;
+
+    public OscillatorController(Oscillator oscillator, BeadsGenerator beadsGenerator) {
+        this.oscillator = oscillator;
+        this.beadsGenerator = beadsGenerator;
+        audioContext = beadsGenerator.getAc();
+        wavePlayer = new WavePlayer(audioContext, 440, Buffer.SAW);
+        audioContext.out.addInput(wavePlayer);
+    }
+
+    public void removeOscillator(Oscillator oscillator) {
+        wavePlayer.kill();
+
+    }
 }
