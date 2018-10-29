4790cad099d6a73599dd622f0bcb46675ebd67fc
andrasSagi <andras.sagi8128@gmail.com>
2018-10-29 12:05:42 +0100

    Add import statements and some fields to BeadsGenerator

--- musicInstallation/src/main/java/music/BeadsGenerator.java
+++ musicInstallation/src/main/java/music/BeadsGenerator.java
@@ -1,4 +1,20 @@
 package music;
 
+import net.beadsproject.beads.core.AudioContext;
+import java.util.ArrayList;
+import java.util.List;
+
 public class BeadsGenerator implements MusicGenerator {
+
+    List<Oscillator> oscillators = new ArrayList<>();
+    private AudioContext ac = new AudioContext();
+
+    AudioContext getAc() {
+        return ac;
+    }
+
+    public void start() {
+        ac.start();
+    }
+
 }
