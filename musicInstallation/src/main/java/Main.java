f2eb1698f4d84c4c18a52795641444b3560da48c
andrasSagi <andras.sagi8128@gmail.com>
2018-10-29 12:04:17 +0100

    Add Exhibition initialization to Main

--- musicInstallation/src/main/java/Main.java
+++ musicInstallation/src/main/java/Main.java
@@ -1,6 +1,10 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
+import music.BeadsGenerator;
+import music.MusicGenerator;
+import visual.*;
 
 public class Main extends Application {
 
@@ -10,9 +14,20 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        Exhibition exhibition = new Exhibition();
+        MusicGenerator beadsGenerator = new BeadsGenerator();
+        final Exhibition exhibition = new Exhibition(beadsGenerator);
+        Button addVisitor = new Button("Create visitor");
+        exhibition.getChildren().add(addVisitor);
+        exhibition.setEntrance(new Entrance(400, 0, exhibition));
+        exhibition.setExit(new Exit(800, 550, exhibition));
+        exhibition.addShowPiece(new ShowPiece(300, 300, exhibition));
+        addVisitor.setOnAction(e -> {
+            new Visitor(exhibition.getEntrance().getX(), exhibition.getEntrance().getY(), exhibition);
+
+        });
         primaryStage.setTitle("Enotronix");
         primaryStage.setScene(new Scene(exhibition, 1000, 700));
         primaryStage.show();
+        exhibition.start();
     }
 }
