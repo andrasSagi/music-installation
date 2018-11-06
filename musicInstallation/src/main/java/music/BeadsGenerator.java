51f1c692877b2577e1ee9a1437b0cd98f0c6dfc8
andrasSagi <andras.sagi8128@gmail.com>
2018-11-06 11:01:11 +0100

    Add constructor, fields and getters to BeadsGenerator

--- musicInstallation/src/main/java/music/BeadsGenerator.java
+++ musicInstallation/src/main/java/music/BeadsGenerator.java
@@ -1,13 +1,23 @@
 package music;
 
 import net.beadsproject.beads.core.AudioContext;
+import net.beadsproject.beads.ugens.Clock;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class BeadsGenerator implements MusicGenerator {
 
-    List<Oscillator> oscillators = new ArrayList<>();
-    private AudioContext ac = new AudioContext();
+    private List<OscillatorController> oscillators = new ArrayList<>();
+    private AudioContext ac;
+    private Clock beatClock;
+
+    public BeadsGenerator() {
+        ac = new AudioContext();
+        beatClock = new Clock(ac, 1500);
+        beatClock.setTicksPerBeat(4);
+        ac.out.addDependent(beatClock);
+    }
 
     AudioContext getAc() {
         return ac;
@@ -17,4 +27,15 @@ public class BeadsGenerator implements MusicGenerator {
         ac.start();
     }
 
+    void removeOscillator(OscillatorController oscillator) {
+        oscillators.remove(oscillator);
+    }
+
+    public int getOscillatorNumber() {
+        return oscillators.size();
+    }
+
+    public Clock getBeatClock() {
+        return beatClock;
+    }
 }
