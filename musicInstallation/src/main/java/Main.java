ccb3c318db3fa3bd6b6414b5f4fdeb9fccc7c45d
andrasSagi <andras.sagi8128@gmail.com>
2018-11-07 11:08:21 +0100

    Add new showpieces

--- musicInstallation/src/main/java/Main.java
+++ musicInstallation/src/main/java/Main.java
@@ -3,7 +3,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 import music.BeadsGenerator;
-import music.MusicGenerator;
+import music.OscillatorController;
 import visual.*;
 
 public class Main extends Application {
@@ -14,20 +14,25 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        MusicGenerator beadsGenerator = new BeadsGenerator();
+        BeadsGenerator beadsGenerator = new BeadsGenerator();
         final Exhibition exhibition = new Exhibition(beadsGenerator);
         Button addVisitor = new Button("Create visitor");
         exhibition.getChildren().add(addVisitor);
         exhibition.setEntrance(new Entrance(400, 0, exhibition));
         exhibition.setExit(new Exit(800, 550, exhibition));
         exhibition.addShowPiece(new ShowPiece(300, 300, exhibition));
+        exhibition.addShowPiece(new ShowPiece(100, 500, exhibition));
+        exhibition.addShowPiece(new ShowPiece(700, 100, exhibition));
+        exhibition.addShowPiece(new ShowPiece(100, 150, exhibition));
+        exhibition.addShowPiece(new ShowPiece(650, 420, exhibition));
         addVisitor.setOnAction(e -> {
-            new Visitor(exhibition.getEntrance().getX(), exhibition.getEntrance().getY(), exhibition);
+            new Visitor(exhibition.getEntrance().getX(), exhibition.getEntrance().getY(), exhibition, new OscillatorController(beadsGenerator));
 
         });
         primaryStage.setTitle("Enotronix");
         primaryStage.setScene(new Scene(exhibition, 1000, 700));
         primaryStage.show();
+        beadsGenerator.start();
         exhibition.start();
     }
 }
