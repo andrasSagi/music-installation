3240b1f71cddc50f03e7fa2a056005661f504bc6
andrasSagi <andras.sagi8128@gmail.com>
2018-11-13 16:22:22 +0100

    Implement Amplifier Module

--- musicInstallation/src/main/java/music/AmpModule.java
+++ musicInstallation/src/main/java/music/AmpModule.java
@@ -1,4 +1,29 @@
 package music;
 
-public class AmpModule {
+import net.beadsproject.beads.core.UGen;
+import net.beadsproject.beads.data.Buffer;
+import net.beadsproject.beads.ugens.Envelope;
+import net.beadsproject.beads.ugens.Gain;
+
+public class AmpModule implements Module {
+
+    private OscillatorController controller;
+    private Envelope envelope;
+    private Gain gain;
+
+    public AmpModule(OscillatorController controller) {
+        this.controller = controller;
+        envelope = new Envelope(controller.getBeadsGenerator().getAc(), 0.6f);
+        gain = new Gain(controller.getBeadsGenerator().getAc(), 1, envelope);
+    }
+
+    @Override
+    public void setInputModule(Module module) {
+        gain.addInput(module.getOutput());
+    }
+
+    @Override
+    public UGen getOutput() {
+        return gain;
+    }
 }
