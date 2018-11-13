833103bcedd9df2f79259feb011a2aacf0a957d5
andrasSagi <andras.sagi8128@gmail.com>
2018-11-13 16:22:47 +0100

    Implement Effect Module

--- musicInstallation/src/main/java/music/EffectModule.java
+++ musicInstallation/src/main/java/music/EffectModule.java
@@ -1,4 +1,29 @@
 package music;
 
-public class EffectModule {
+import net.beadsproject.beads.core.UGen;
+import net.beadsproject.beads.ugens.Gain;
+import net.beadsproject.beads.ugens.Reverb;
+
+public class EffectModule implements Module {
+
+    private OscillatorController controller;
+    private Reverb reverb;
+    private Gain gain;
+
+    public EffectModule(OscillatorController controller) {
+        this.controller = controller;
+        reverb = new Reverb(controller.getAudioContext(), 1);
+        gain = new Gain(controller.getAudioContext(), 1, 0.6f);
+        gain.addInput(reverb);
+    }
+
+    @Override
+    public void setInputModule(Module module) {
+        reverb.addInput(module.getOutput());
+    }
+
+    @Override
+    public UGen getOutput() {
+        return gain;
+    }
 }
