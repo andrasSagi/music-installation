37e329b9aa2c8da327704d1f2bdbd74f0b67029c
andrasSagi <andras.sagi8128@gmail.com>
2018-11-13 13:06:41 +0100

    Add tempoInMSec

--- musicInstallation/src/main/java/music/BeadsGenerator.java
+++ musicInstallation/src/main/java/music/BeadsGenerator.java
@@ -3,6 +3,7 @@ package music;
 import net.beadsproject.beads.core.AudioContext;
 import net.beadsproject.beads.ugens.Clock;
 
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,21 +11,32 @@ public class BeadsGenerator implements MusicGenerator {
 
     private List<OscillatorController> oscillators = new ArrayList<>();
     private AudioContext ac;
+    private float tempoInMSec = 1500;
     private Clock beatClock;
 
     public BeadsGenerator() {
         ac = new AudioContext();
-        beatClock = new Clock(ac, 1500);
+        beatClock = new Clock(ac, tempoInMSec);
         beatClock.setTicksPerBeat(4);
         ac.out.addDependent(beatClock);
+        //Gain master = new Gain(ac, 1, 0.8f);
+        ac.out.setGain(0.8f);
     }
 
     AudioContext getAc() {
         return ac;
     }
 
+    public float getTempoInMSec() {
+        return tempoInMSec;
+    }
+
     public void start() {
         ac.start();
+    }
+
+    void addOscillator(OscillatorController oscillator) {
+        oscillators.add(oscillator);
     }
 
     void removeOscillator(OscillatorController oscillator) {
